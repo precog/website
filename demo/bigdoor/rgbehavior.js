2c698fdee660d995460fcdf7cdf8f4bcd0da7b0b
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-07-15 14:09:43 -0600

    fixin loggedin

--- demo/bigdoor/rgbehavior.js
+++ demo/bigdoor/rgbehavior.js
@@ -1,5 +1,5 @@
 (function() {
-function dump(o) { if(typeof(o) == 'object') { var buf = "{"; for(f in o) buf += f + ":" + dump(o[f]); return buf + "}"; } else return ""+o; }
+function dump(o) { if(typeof(o) == 'object') { var buf = []; for(f in o) buf.push(f + ":" + dump(o[f])); return "{" + buf.join(",") + "}"; } else return ""+o; }
 $.cookie = function (key, value, options) {if (arguments.length > 1 && String(value) !== "[object Object]") {options = jQuery.extend({}, options);if (value === null || value === undefined) {options.expires = -1;}if (typeof options.expires === 'number') {var days = options.expires, t = options.expires = new Date();t.setDate(t.getDate() + days);} value = String(value);return (document.cookie = [encodeURIComponent(key), '=',options.raw ? value : encodeURIComponent(value),options.expires ? '; expires=' + options.expires.toUTCString() : '',options.path ? '; path=' + options.path : '',options.domain ? '; domain=' + options.domain : '',options.secure ? '; secure' : ''].join(''));}options = value || {};var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;};
 
 var host = window.location.hostname.replace(/\./g, '_'),
@@ -33,7 +33,8 @@ function elapsed()
 }
 
 BDM.auth.status(function(status) {
-loggedin = !!status;
+console.log(status);
+loggedin = null != status;
 // TIME ON PAGE
 elapsed();
 
