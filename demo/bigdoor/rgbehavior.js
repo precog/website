1186c4d8a7c3e34210521be8f22b7193936ea5ea
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-07-15 11:49:55 -0600

    update

--- demo/bigdoor/rgbehavior.js
+++ demo/bigdoor/rgbehavior.js
@@ -1,19 +1,9 @@
-// ==UserScript==
-// @name           ReportGrid for BigDoor
-// @namespace      http://www.reportgrid.com/c/bigdoor/behavior
-// @include        http://*.bigdoor.com/*
-// @description    Tracks User Behavior
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
-// ==/UserScript==
-
 (function() {
 function dump(o) { if(typeof(o) == 'object') { var buf = "{"; for(f in o) buf += f + ":" + dump(o[f]); return buf + "}"; } else return ""+o; }
 $.cookie = function (key, value, options) {if (arguments.length > 1 && String(value) !== "[object Object]") {options = jQuery.extend({}, options);if (value === null || value === undefined) {options.expires = -1;}if (typeof options.expires === 'number') {var days = options.expires, t = options.expires = new Date();t.setDate(t.getDate() + days);} value = String(value);return (document.cookie = [encodeURIComponent(key), '=',options.raw ? value : encodeURIComponent(value),options.expires ? '; expires=' + options.expires.toUTCString() : '',options.path ? '; path=' + options.path : '',options.domain ? '; domain=' + options.domain : '',options.secure ? '; secure' : ''].join(''));}options = value || {};var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;};
 
-
-var ReportGrid = window.ReportGrid || { track : function(path, o) { console.log("track: " + path + " " + dump(o)); } },
-	host = window.location.hostname.replace(/\./g, '_'),
-	path = "/test/bigdoor/" + host,
+var host = window.location.hostname.replace(/\./g, '_'),
+	path = "/bdtest/bigdoor/" + host,
 	cvisit = "rgbd-visits",
 	previous_visits = $.cookie(cvisit) || 0,
 	loggedin = false;
@@ -30,9 +20,8 @@ function event(name, properties) {
 
 $(document).ready(function() { window.setTimeout(function() {
 
-BDM.auth.status(function(d) {
-
-loggedin = true && d;
+BDM.auth.status(function(status) {
+loggedin = !!status;
 // ENGAGEMENT
 ReportGrid.track(path, event("engagement", { previousVisits : previous_visits }));
 // FIRST ENGAGEMENT
