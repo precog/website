97b9de67c5e19a9be775775f92fbcf6e7dee4313
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-08-08 16:53:35 -0600

    changed path for bigdoor data

--- demo/bigdoor/js/rgbehavior.js
+++ demo/bigdoor/js/rgbehavior.js
@@ -3,8 +3,8 @@ function dump(o) { if(typeof(o) == 'object') { var buf = []; for(f in o) buf.pus
 $.cookie = function (key, value, options) {if (arguments.length > 1 && String(value) !== "[object Object]") {options = jQuery.extend({}, options);if (value === null || value === undefined) {options.expires = -1;}if (typeof options.expires === 'number') {var days = options.expires, t = options.expires = new Date();t.setDate(t.getDate() + days);} value = String(value);return (document.cookie = [encodeURIComponent(key), '=',options.raw ? value : encodeURIComponent(value),options.expires ? '; expires=' + options.expires.toUTCString() : '',options.path ? '; path=' + options.path : '',options.domain ? '; domain=' + options.domain : '',options.secure ? '; secure' : ''].join(''));}options = value || {};var result, decode = options.raw ? function (s) { return s; } : decodeURIComponent;return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;};
 var BrowserDetect = {init: function () {this.browser = this.searchString(this.dataBrowser) || "An unknown browser";this.version = this.searchVersion(navigator.userAgent)|| this.searchVersion(navigator.appVersion)|| "an unknown version";this.OS = this.searchString(this.dataOS) || "an unknown OS";},searchString: function (data) {for (var i=0;i<data.length;i++)	{var dataString = data[i].string;var dataProp = data[i].prop;this.versionSearchString = data[i].versionSearch || data[i].identity;if (dataString) {if (dataString.indexOf(data[i].subString) != -1)return data[i].identity;}else if (dataProp)return data[i].identity;}},searchVersion: function (dataString) {var index = dataString.indexOf(this.versionSearchString);if (index == -1) return;return parseFloat(dataString.substring(index+this.versionSearchString.length+1));},dataBrowser: [{string: navigator.userAgent,subString: "Chrome",identity: "Chrome"},{ 	string: navigator.userAgent,subString: "OmniWeb",versionSearch: "OmniWeb/",identity: "OmniWeb"},{string: navigator.vendor,subString: "Apple",identity: "Safari",versionSearch: "Version"},{prop: window.opera,identity: "Opera"},{string: navigator.vendor,subString: "iCab",identity: "iCab"},{string: navigator.vendor,subString: "KDE",identity: "Konqueror"},{string: navigator.userAgent,subString: "Firefox",identity: "Firefox"},{string: navigator.vendor,subString: "Camino",identity: "Camino"},{string: navigator.userAgent,subString: "Netscape",identity: "Netscape"},{string: navigator.userAgent,subString: "MSIE",identity: "Explorer",versionSearch: "MSIE"},{string: navigator.userAgent,subString: "Gecko",identity: "Mozilla",versionSearch: "rv"},{string: navigator.userAgent,subString: "Mozilla",identity: "Netscape",versionSearch: "Mozilla"}],dataOS : [{string: navigator.platform,subString: "Win",identity: "Windows"},{string: navigator.platform,subString: "Mac",identity: "Mac"},{string: navigator.userAgent,subString: "iPhone",identity: "iPhone/iPod"},{string: navigator.platform,subString: "Linux",identity: "Linux"}]};BrowserDetect.init();
 
-var host = window.location.hostname.replace(/\./g, '_'),
-	paths = ["/bigdoor/", "/bigdoor/" + host],
+var host = window.location.hostname, //.replace(/\./g, '_'),
+	paths = ["/", "/" + host],
 	cvisit = "rgbd-visits", clogin = "rgbd-loggedin",
 	previous_visits = $.cookie(cvisit) || 0,
 	loggedin = false,
@@ -29,13 +29,7 @@ function track(event)
 		badges.forEach(function(badge) {
 			p["badge_" + badge.pub_title.toLowerCase().replace(/ /g, '_')] = true;
 		});
-		for(var i = 0; i < paths.length; i++)
-		{
-			var debug = (function(path) { return function(r) { console.log("track:" + path + " " + dump(e)); }; })(paths[i]);
-			ReportGrid.track(paths[i], { events : e }
-//				, debug
-			);
-		}
+		ReportGrid.track(paths, { events : e });
 	});
 	});
 }
