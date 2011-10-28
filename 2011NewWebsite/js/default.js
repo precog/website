4ee2e9c70d1c1492556f1fe71d0147e17c8ad0cf
John A. De Goes <john@degoes.net>
2011-10-28 00:33:48 -0600

    fix password

--- 2011NewWebsite/js/default.js
+++ 2011NewWebsite/js/default.js
@@ -1,3 +1,4 @@
+// JSON
 (function($){var escapeable=/["\\\x00-\x1f\x7f-\x9f]/g,meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'};$.toJSON=typeof JSON==='object'&&JSON.stringify?JSON.stringify:function(o){if(o===null){return'null';}
 var type=typeof o;if(type==='undefined'){return undefined;}
 if(type==='number'||type==='boolean'){return''+o;}
@@ -20,6 +21,9 @@ val=$.toJSON(o[k]);pairs.push(name+':'+val);}
 return'{'+pairs.join(',')+'}';}};$.evalJSON=typeof JSON==='object'&&JSON.parse?JSON.parse:function(src){return eval('('+src+')');};$.secureEvalJSON=typeof JSON==='object'&&JSON.parse?JSON.parse:function(src){var filtered=src.replace(/\\["\\\/bfnrtu]/g,'@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']').replace(/(?:^|:|,)(?:\s*\[)+/g,'');if(/^[\],:{}\s]*$/.test(filtered)){return eval('('+src+')');}else{throw new SyntaxError('Error parsing JSON, source is not valid.');}};$.quoteString=function(string){if(string.match(escapeable)){return'"'+string.replace(escapeable,function(a){var c=meta[a];if(typeof c==='string'){return c;}
 c=a.charCodeAt();return'\\u00'+Math.floor(c/16).toString(16)+(c%16).toString(16);})+'"';}
 return'"'+string+'"';};})(jQuery);
+
+// USTORE
+var USTORE=(function(){var e,a,c,f,b,k,i,j,d;var g={setValue:function(l,m,n){if(e){if(n&&a){sessionStorage.setItem(l,m)}else{localStorage.setItem(l,m)}}else{if(c){if(n){i.setAttribute(l,m);i.save(d)}else{f.setAttribute(l,m);f.save(ieDb)}}}},getValue:function(m,n){var l="";if(e){if(n&&a){l=sessionStorage.getItem(m)}else{l=localStorage.getItem(m)}}else{if(c){if(n){i.load(d);l=i.getAttribute(m)}else{f.load(ieDb);l=f.getAttribute(m)}}}return l},deleteValue:function(l,m){if(e){this.setValue(l,null,m)}else{if(c){if(m){i.removeAttribute(l);i.save(d)}else{f.removeAttribute(l);f.save(ieDb)}}}},clearDB:function(l){if(e){if(l){sessionStorage.clear()}else{localStorage.clear()}}else{if(c){h.clearDB(l)}}}};var h={detectIE:function(){if(/MSIE (\d+\.\d+);/.test(navigator.userAgent)){var l=new Number(RegExp.$1);if(l>=5.5&&l<=8){return true}}return false},init:function(){var n=document.createElement("meta");n.name="save";n.content="userdata";document.getElementsByTagName("head").item(0).appendChild(n);var m=new Date().getTime();var l=document.createElement("div");b="ie-db-"+m;ieDb="userStorage";l.setAttribute("id",b);body.appendChild(l);f=document.getElementById(b);f.style.behavior="url('#default#userData')";f.style.display="none";if(window.name===null||window.name===undefined||window.name===""){window.name="ie-sesh-db-"+m}j=window.name;d=j;l=document.createElement("div");l.setAttribute("id",j);f.appendChild(l);i=document.getElementById(j);i.style.behavior="url('#default#userData')";i.style.display="none"},clearDB:function(r){var m=new Date().getTime(),t=document.createElement("div"),l=r?i:f,p=r?d:ieDb,s=l.xmlDocument,n=s.firstChild.attributes,q,o=n.length;while(0<=--o){q=n[o];l.removeAttribute(q.nodeName)}l.save(p)}};return{init:function(){if(typeof(window.localStorage)==="object"){e=true;try{if(typeof(window.sessionStorage)==="object"){a=true}}catch(l){a=false}}else{if(h.detectIE()){c=true;h.init()}}},setValue:function(l,m){g.setValue(l,m,false)},setSessionValue:function(l,m){g.setValue(l,m,true)},getValue:function(l){return g.getValue(l,false)},getSessionValue:function(l){return g.getValue(l,true)},deleteValue:function(l){g.deleteValue(l,false)},deleteSessionValue:function(l){g.deleteValue(l,true)},clearLocalStorage:function(){g.clearDB(false)},clearSessionStorage:function(){g.clearDB(true)},clearDOMStorage:function(){g.clearDB(false);g.clearDB(true)}}})();
 
 var API = {};
 
@@ -441,6 +445,8 @@ var API = {};
 })();
 
 $(function() {
+  var RootAccountsAPI = 'https://api.reportgrid.com/services/billing/v1/accounts/';
+
   var setupHome = function() {
     var animateContentPane = function(panelIndex) {
       return function() {
@@ -480,7 +486,29 @@ $(function() {
     $('#loginpopupbutton').click(function(e) {
       e.preventDefault();
 
+      var email    = $('#loginform input[name="email"]');
+      var password = $('#loginform input[name="password"]');
 
+      API.Http.post(RootAccountsAPI + "get", {
+        email:      email.val(),
+        password:   password.val()
+      }, {
+        success: function(response) {
+          var content = $('#middlecontent');
+
+          var tokenId = response.id.token;
+
+          USTORE.setSessionValue('email',    email.val());
+          USTORE.setSessionValue('password', password.val());
+          USTORE.setSessionValue('tokenId',  tokenId);
+
+          window.location = "./control-panel.html";
+        },
+
+        failure: function(code, text) {
+          alert(text);
+        }
+      });
 
       return false;
     });
@@ -579,8 +607,6 @@ $(function() {
   }
 
   var setupAccountCreation = function() {
-    var RootAPI = 'https://api.reportgrid.com/services/billing/v1/accounts/';
-
     try {
       $("#signupForm").validate({
         rules: {
@@ -716,7 +742,7 @@ $(function() {
 
       console.log(request);
 
-      API.Http.post(RootAPI, request, {
+      API.Http.post(RootAccountsAPI, request, {
         success: function(response) {
           var content = $('#middlecontent');
 
