da15fb255d28fea36e690573e594efb9bac7f894
Matthew T De Goes <matthew@reportgrid.com>
2011-11-07 10:21:13 -0700

    API Documentation Update

--- js/default.js
+++ js/default.js
@@ -792,7 +792,7 @@ $(function() {
   }
   
   var setupSyntaxHighlighting = function() {
-    $("pre.literal-block").snippet("css",{style:"darkness"});
+    $("pre.literal-block").snippet("css",{style:".sh_darkness"});
    $("pre.literal-block").snippet("javascript",{style:"random",transparent:true,showNum:false});
   }
 
