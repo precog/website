7fc7af054d48936f9918454261525eb926e2a25f
Matthew T De Goes <matthew@reportgrid.com>
2011-12-19 15:04:19 -0700

    New Single Page Site Queriy

--- queriowebsite/js/default.js
+++ queriowebsite/js/default.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+  
+  /*
   function swapImage(index) {
     $(".appselector img").attr("src", "imagesquerio/yourappdot.png");
     $(".appselector:nth-child("+index+") img").attr("src", "imagesquerio/yourappdot-alt.png");
@@ -12,4 +14,33 @@ $(document).ready(function(){
     })
     return false;
   });
+  
+  $(".yoxview").yoxview({
+    autoHideInfo : false,
+  });
+  */
+  var slides = 3,
+      width  = 1090,
+      index  = 0;
+  function slide()
+  {
+    $("#leftarrow").attr("src", index == 0 ? "/imagesquerio/leftarrow-alt.png" : "/imagesquerio/leftarrow.png");
+    $("#rightarrow").attr("src", index == slides-1 ? "/imagesquerio/rightarrow-alt.png" : "/imagesquerio/rightarrow.png");
+    $("#appslider").animate({
+      left : - index * width
+    })
+  }
+  $("#rightarrow").click(function(){
+    if(index == slides-1)
+      return;
+    index++;
+    slide();
+  })
+  $("#leftarrow").click(function(){
+    if(index == 0)
+      return;
+    index--;
+    slide();
+  })
 })
+
