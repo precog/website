a3150c381d490e470393657ae813db35375e36f3
Matthew T De Goes <matthew@reportgrid.com>
2012-02-28 09:08:44 -0700

    Precog Site Update

--- queriowebsite/js/console.js
+++ queriowebsite/js/console.js
@@ -35,47 +35,6 @@ var console = $('#console'),
 $('#console-execute-button').click(function() {
   controller.trigger();
 })
-/*
-    editor = ace.edit("console");
-    editor.commands.addCommand({
-        name: 'executeQuery',
-        bindKey: {
-            win: 'Shift-Return',
-            mac: 'Shift-Return|Command-Return',
-            sender: 'editor|cli'
-        },
-        exec: function(env, args, request) {
-          executeQuery();
-        }
-    });
-    editor.setTheme("ace/theme/pastel_on_dark");
-    editor.setShowPrintMargin(false);
-    var QuirrelMode = require("ace/mode/quirrel").Mode;
-    editor.getSession().setMode(new QuirrelMode());
 
-    var output = $("#console-output");
-
-    var displayOutput = function(text, cls)
-    {
-      output.append('<div class="msg '+cls+'">'+text+'</div>');
-      output.animate({ scrollTop: output.prop("scrollHeight") }, 250);
-    }
-    var success = function(response) {
-      displayOutput(response, "success");
-    }
-
-    var error = function(message) {
-      displayOutput(message, "error");
-    }
-
-    var executeQuery = function()
-    {
-      var content = editor.getSession().getValue();
-      setTimeout(function() {
-        Math.random() < 0.5 ? success('{ a : 1 }') : error("doesn't work");
-      }, 100) // replace with POST to service
-    }
-    $('#console-execute-button').click(executeQuery);
-*/
 })
 
